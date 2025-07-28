1242e638075b3651c822415949602ac930b7e0ed
UndeadRonin99 <ST10046014@vcconnect.edu.za>
2025-07-28 15:35:49 +0200

    adding activities

--- app/src/main/java/com/nicholas/application_test_hive/AddHabitActivity.kt
+++ app/src/main/java/com/nicholas/application_test_hive/AddHabitActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class AddHabitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_habit)
 
     }
 }
